ccf03bc92d3247c7f3885447aee62529c6cabd28
felixfernandez <felixangel.fernandez@gmail.com>
2014-11-20 20:27:26 +0100

    assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## author: Félix Ángel Fernández Alonso
+## Functions: MakeCacheMatrix and CacheSolve
+## Date: 20 November 2014
+## Course: DataScience Specialization
+## Description: The functions belows solve different problems 
+## about how calculate an inverse matrix.
 
-## Write a short comment describing this function
+## Function: MakeCacheMatrix
+## Description: This function generate a special matrix as
+## a parameter of MakeCacheMatrix. Inside the function
+## we create different list objects as part of a list doing the next:
+##  set --> set the value of the matrix
+##  get --> get the value of the matrix
+##  setinver --> set the value of the matrix inverse
+##  getinver --> get the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+        m<-NULL
+        set <- function(y = matrix()) {
+            x <<- y
+            m<-NULL
+        }
+        get <- function() x
+        setinver <- function(solve) m <<- solve
+        getinver <- function() m
+        list(set = set, get = get,
+             setinver = setinver,
+             getinver = getinver)
 
 }
 
-
-## Write a short comment describing this function
+## Function: cacheSolve
+## Description: This function calculate the inverse matrix
+## using the matrix created by makeCacheMatrix function above
+## one of the steps is verify if the inverse matrix value already
+## exist, in this case return the cache value and skip calculate it.
+## if the function doesn't find the value calculate the inverse matrix
+## using the function setinver.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinver()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinver(m)
+    m
 }
+
+## with real inverse matrix works, examples are like this to test
+## the function:
+## mat <- matrix(1:4, 2,2)
+## matObj <- makeCacheMatrix(mat)
+## cachesolve(matObj)
